a59ad17370a1153049e867acaeba173cca671e51
Theerapat Linprasert <boatrocker123@gmail.com>
2025-02-07 14:31:41 +0700

    update database fix env

---
+++ backend/src/api/category-car/controllers/category-car.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-car controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::category-car.category-car');
