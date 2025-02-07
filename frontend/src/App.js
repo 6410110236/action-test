e7fb546fe2559ba63d0fefb17f3dfe29aac67a4a
Thanawat Thipkaew <6710110177@psu.ac.th>
2025-02-07 12:21:00 +0700

    Refactor file structure and update paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home'; 
-import About from './pages/About'; 
-import Users from './pages/Users'; 
+import Home from './modules/landing/pages/Home'; 
+import Users from './modules/auth/pages/Users'; 
 import Header from './layout/Header'; 
-import signIn from './pages/signIn';
-import signUp from './pages/signUp';
+import signIn  from './modules/auth/pages/signIn';
+import signUp from './modules/auth/pages/signUp';
 
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Header isLoggedIn={isLoggedIn} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/user" element={<Users />} />
           <Route path="/signin" element={<signIn />} />
           <Route path="/signup" element={<signUp />} />
