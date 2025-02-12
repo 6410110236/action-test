27cfb1aeac527e2c9ec751daaf149c8b83f1a1b8
PhattharinK <6710110282@psu.ac.th>
2025-02-12 18:22:01 +0700

    feat: create sign-up page

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../modules/landing/pages/Home';
 import SignIn from '../modules/auth/pages/signIn';
-import signUp from '../modules/auth/pages/signUp';
+import SignUp from '../modules/auth/pages/signUp';
 import Users from '../modules/transaction/components/SellUser';
 
 const AppRoutes = () => {
@@ -11,7 +11,7 @@ const AppRoutes = () => {
             <Route path="/" element={<Home />} />
             <Route path="/user" element={<Users />} />
             <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<signUp />} />
+            <Route path="/signup" element={<SignUp />} />
         </Routes>
     );
 };
