fd97c392a282db158451597de1541ea70b01a843
Thanawat Thipkaew <6710110177@psu.ac.th>
2025-03-04 15:17:33 +0700

    Fix:config for deployment

--- frontend/src/api/main.js
+++ frontend/src/api/main.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
-
+import config from "../config";
 const conf = {
-  apiUrlPrefix: process.env.REACT_APP_API_URL || "http://localhost:1337", // Add default API URL
+  apiUrlPrefix: config.serverUrlPrefix, // Add default API URL
   loginEndpoint: "/api/auth/local", // Verify this endpoint matches your backend
   jwtUserEndpoint: "/api/users/me?populate=*",
   jwtSessionStorageKey: "auth.jwt",
