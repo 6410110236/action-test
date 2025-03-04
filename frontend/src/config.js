5c2a722d2d5b061dc360bd9b61198417f2f96692
Theerapat Linprasert <boatrocker123@gmail.com>
2025-03-04 21:05:00 +0700

    update frontend : fix path url in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
   isProd,
-  serverUrlPrefix: isProd ? 'https://w07-admin.pupasoft.com' : 'http://localhost:1337/api', //*****change domain name
+  serverUrlPrefix: isProd ? 'https://w07-admin.pupasoft.com' : 'http://localhost:1337', //*****change domain name
   STRIPE_PUBLISHABLE_KEY : process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY,
   STRIPE_SECRET_KEY : process.env.REACT_APP_STRIPE_SECRET_KEY,
   STRIPE_SUCCESS_URL : process.env.REACT_APP_STRIPE_SUCCESS_URL,
