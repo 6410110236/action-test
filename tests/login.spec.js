2be288fbba5c65fffc82204b6f7c63b883e2c4fb
PhattharinK <6710110282@psu.ac.th>
2025-03-07 13:30:26 +0700

    login: testcase and navigation

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test("login with correct credentials", async ({ page }) => {
+test("login with correct credentials", async ({ page, browserName }) => {
   await page.goto('https://w07.pupasoft.com/signin');
   await page.fill('input[name="username"]', 'buyer1');
-  await page.fill('input[name="password"]', 'password');
-  await page.click('button[type="submit"]');
-  await page.waitForURL('https://w07.pupasoft.com/home');
-  await page.goto('https://w07.pupasoft.com/user');
-  await expect(page).toHaveURL('https://w07.pupasoft.com/user');
+  if (browserName === 'webkit') {
+    await page.fill('input[name="password"]', 'password');
+    await page.press('input[name="password"]', 'Enter');
+  } else {
+    await page.fill('input[name="password"]', 'password');
+    await page.click('button[type="submit"]');
+  }
+  console.log('Current URL:', await page.url());
+  await expect(page).toHaveURL('https://w07.pupasoft.com/home');
 });
 
 test("login with incorrect credentials", async ({ page }) => {
