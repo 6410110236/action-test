c7a68a7a9f7e50b4b12bb537d0652f3f76231a10
PhattharinK <6710110282@psu.ac.th>
2025-03-07 11:48:48 +0700

    fix: syntax error

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -10,11 +10,11 @@ test("login with correct credentials", async ({ page }) => {
   await expect(page).toHaveURL('https://w07.pupasoft.com/user');
 });
 
-test("login with incorrect credentials"), async ({ page }) => {
+test("login with incorrect credentials", async ({ page }) => {
   await page.goto('https://w07.pupasoft.com/signin');
   await page.fill('input[name="username"]', 'buyer1');
   await page.fill('input[name="password"]', 'wrongpassword');
   await page.click('button[type="submit"]');
   await page.waitForSelector('.alert-danger');
   await expect(page).toHaveText('.alert-danger', 'Invalid credentials');
-}
+});
