6f615d92a94dc2c0603634aab2b6324b7d164675
PhattharinK <6710110282@psu.ac.th>
2025-03-07 11:45:12 +0700

    add: login test via playwright

---
+++ tests/login.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test("login with correct credentials", async ({ page }) => {
+    await page.goto('https://w07.pupasoft.com/signin');
+    await page.fill('input[name="username"]', 'buyer1');
+    await page.fill('input[name="password"]', 'password');
+    await page.click('button[type="submit"]');
+    await page.waitForURL('https://w07.pupasoft.com/home');
+    await page.goto('https://w07.pupasoft.com/user');
+    await expect(page).toHaveURL('https://w07.pupasoft.com/user');
+  });
