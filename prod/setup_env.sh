a7efd03e3343f160863de90720c0ade50555f1c0
Theerapat Linprasert <boatrocker123@gmail.com>
2025-03-26 16:33:52 +0700

    update github workflow 5

--- prod/setup_env.sh
+++ prod/setup_env.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-echo -e "HOST=${{ secret.HOST }}" >> .env
-echo -e "PORT=${{ secret.PORT }}" >> .env
+# #!/bin/bash
+# echo -e "HOST=${{ secret.HOST }}" >> .env
+# echo -e "PORT=${{ secret.PORT }}" >> .env
 
-echo -e "NODE_ENV=${{ secret.NODE_ENV }}" >> .env
+# echo -e "NODE_ENV=${{ secret.NODE_ENV }}" >> .env
 
-echo -e "APP_KEYS=${{ secret.APP_KEYS }}" >> .env
-echo -e "API_TOKEN_SALT=${{ secret.API_TOKEN_SALT }}" >> .env
-echo -e "ADMIN_JWT_SECRET=${{ secret.ADMIN_JWT_SECRET }}" >> .env
-echo -e "TRANSFER_TOKEN_SALT=${{ secret.TRANSFER_TOKEN_SALT }}" >> .env
+# echo -e "APP_KEYS=${{ secret.APP_KEYS }}" >> .env
+# echo -e "API_TOKEN_SALT=${{ secret.API_TOKEN_SALT }}" >> .env
+# echo -e "ADMIN_JWT_SECRET=${{ secret.ADMIN_JWT_SECRET }}" >> .env
+# echo -e "TRANSFER_TOKEN_SALT=${{ secret.TRANSFER_TOKEN_SALT }}" >> .env
 
-echo -e "DATABASE_CLIENT=${{ secret.DATABASE_CLIENT }}" >> .env
-echo -e "DATABASE_HOST=${{ secret.DATABASE_HOST }}" >> .env
-echo -e "DATABASE_PORT=${{ secret.DATABASE_PORT }}" >> .env
-echo -e "DATABASE_NAME=${{ secret.DATABASE_NAME }}" >> .env
-echo -e "DATABASE_USERNAME=${{ secret.DATABASE_USERNAME }}" >> .env
-echo -e "DATABASE_PASSWORD=${{ secret.DATABASE_PASSWORD }}" >> .env
-echo -e "DATABASE_SSL=${{ secret.DATABASE_SSL }}" >> .env
-echo -e "DATABASE_FILENAME=${{ secret.DATABASE_FILENAME }}" >> .env
-echo -e "JWT_SECRET=${{ secret.JWT_SECRET }}" >> .env
+# echo -e "DATABASE_CLIENT=${{ secret.DATABASE_CLIENT }}" >> .env
+# echo -e "DATABASE_HOST=${{ secret.DATABASE_HOST }}" >> .env
+# echo -e "DATABASE_PORT=${{ secret.DATABASE_PORT }}" >> .env
+# echo -e "DATABASE_NAME=${{ secret.DATABASE_NAME }}" >> .env
+# echo -e "DATABASE_USERNAME=${{ secret.DATABASE_USERNAME }}" >> .env
+# echo -e "DATABASE_PASSWORD=${{ secret.DATABASE_PASSWORD }}" >> .env
+# echo -e "DATABASE_SSL=${{ secret.DATABASE_SSL }}" >> .env
+# echo -e "DATABASE_FILENAME=${{ secret.DATABASE_FILENAME }}" >> .env
+# echo -e "JWT_SECRET=${{ secret.JWT_SECRET }}" >> .env
